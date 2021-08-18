af1e73ab3a09d5345c9aa5035ef125e2cbadd4cf
Em Sparr <amberem@gmail.com>
2021-08-18 09:30:48 -0400

    Models updated. One book added to database.

--- books/models.py
+++ books/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
         regex=r'^\d{4}$',
         message="Album year must be entered in the format: 'XXXX'.")
     title = models.CharField(max_length=255, null=True, blank=True)
-    author = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ManyToManyField("Author", related_name="author")
     publisher = models.CharField(max_length=255, null=True, blank=True)
     year_published = models.CharField(max_length=4,
                             validators=[year_regex],
@@ -24,12 +24,12 @@ class Book(models.Model):
     description = models.TextField(null=True, blank=True)
     url = models.URLField(null=True, blank=True)
     image_url = models.URLField(null=True, blank=True)
-    categories = models.ManyToManyField("Category", related_name="books")
+    categories = models.ManyToManyField("Category", related_name="category")
     created_at = models.DateTimeField(auto_now_add=True)
-    favorited_by = models.ManyToManyField("User", related_name="fav_books")
+    favorited_by = models.ManyToManyField("User", related_name="fav_books", null=True, blank=True)
 
     def __repr__(self):
-        return f"<Album title={self.title}>"
+        return f"<Book title={self.title}>"
     
     def __str__(self):
         return self.title
@@ -41,6 +41,16 @@ class Category(models.Model):
     def __repr__(self):
         return f"<Category name={self.name}>"
     
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField("Book", related_name="book")
+
+    def __repr__(self):
+        return f"<Author name={self.name}>"
+
     def __str__(self):
         return self.name
 
